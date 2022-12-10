4f8f300f361861439590408b8307176813f110c3
plamen565 <plamen565@gmail.com>
2022-12-10 13:49:12 +0200

    Add batMaterial test

---
+++ Baseball.Tests/ServicesTests/BatMaterialServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using Baseball.Common.ViewModels.BatViewModels;
+using Baseball.Core.Servises;
+using Baseball.Infrastructure.Repository;
+
+namespace Baseball.UnitTests.ServicesTests
+{
+    public class BatMaterialServiceTests
+    {
+        private InMemoryDatabase inMemoryDatabase;
+        private IRepository repository;
+        private BatMaterialService batMaterialService;
+
+        [SetUp]
+        public void Setup()
+        {
+            inMemoryDatabase = new InMemoryDatabase();
+            repository = new Repository(inMemoryDatabase.Context);
+            batMaterialService = new BatMaterialService(repository);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            inMemoryDatabase.Context.Dispose();
+        }
+
+        [Test]
+        public async Task GetAll_ShouldReturnAllSuccessfully()
+        {
+            var all = await batMaterialService.GetAllBatMaterialsAsync();
+
+            Assert.IsNotNull(all);
+            Assert.IsTrue(all.Count() == 2);
+            Assert.IsTrue(all.First().GetType() == typeof(BatMaterialViewModel));
+        }
+    }
+}
